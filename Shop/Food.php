eb9e6c2557255084c91d6e7f12c843f94c33bc40
Stefano-Bernini <stefanobernini26@gmail.com>
2023-07-17 01:50:51 +0200

    collegamenti index e creata classe food

--- Shop/Food.php
+++ Shop/Food.php
@@ -1,3 +1,12 @@
 <?php
+    class Food extends Product{
+        public $weight;
+        public $ingredients;
 
+        public function __construct(String $_name, String $_image, Float $_price, String $_category, Float $_weight, String $_ingredients){
+            parent::__construct($_name, $_image, $_price, $_category);
+            $this->weight = $_weight;
+            $this->ingredients = $_ingredients;
+        }
+    }
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
+    require_once __DIR__.'/Shop/Product.php';
 
+    $product = new Product('Test prodotto', 'https://static.zoomalia.com/prod_img/11540/lm_66584117275be999ff55a987b9381e01f961581340929.jpg', 34.10, 'Cane');
+
+    var_dump($product);
 ?>
 
 
