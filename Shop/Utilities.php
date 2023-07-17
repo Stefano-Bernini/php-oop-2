b91564b3fa9f74ba1e9a24bb6dc146a3dd3986be
Stefano-Bernini <stefanobernini26@gmail.com>
2023-07-17 02:15:21 +0200

    create classi game e utilities

--- Shop/Utilities.php
+++ Shop/Utilities.php
@@ -1,3 +1,12 @@
 <?php
+    class Utilities extends Product{
+        public $materials;
+        public $size;
 
+        public function __construct(String $_name, String $_image, Float $_price, String $_category, Float $_size, String $_materials){
+            parent::__construct($_name, $_image, $_price, $_category);
+            $this->size = $_size;
+            $this->materials = $_materials;
+        }
+    }
 ?>
--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
     require_once __DIR__.'/Shop/Product.php';
+    require_once __DIR__.'/Shop/Food.php';
 
     $product = new Product('Test prodotto', 'https://static.zoomalia.com/prod_img/11540/lm_66584117275be999ff55a987b9381e01f961581340929.jpg', 34.10, 'Cane');
+    $food = new Food('Test prodotto', 'https://static.zoomalia.com/prod_img/11540/lm_66584117275be999ff55a987b9381e01f961581340929.jpg', 23.99, 'Gatto', 10, 'Tonno, Pollo, Prosciutto');
 
     var_dump($product);
+    var_dump($food);
 ?>
 
 
